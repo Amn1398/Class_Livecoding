7b596d8636129528f02dd13b71d6164bbe40afde
Avi <newma1@umbc.edu>
2020-02-25 15:27:22 -0500

    Class Stuff

--- squirtle_facts.py
+++ squirtle_facts.py
@@ -3,7 +3,8 @@ import json
 if __name__ == '__main__':
     with open("squirtle.json", 'r') as f:
         squirtle_facts = json.load(f)
-        print(json.dumps(squirtle_facts, indent=2))
+        #print(json.dumps(squirtle_facts, indent=2))
+        print(json.dumps(squirtle_facts["stats"], indent=2))
         for ability in squirtle_facts['abilities']:
             print(ability["ability"]["name"])
         for move_dict in squirtle_facts['moves']:
