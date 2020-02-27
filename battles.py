c5fdf8f68da43eff07eefbd65a598d4995f54fe0
Avi <newma1@umbc.edu>
2020-02-27 15:13:43 -0500

    Class Stuff

--- battles.py
+++ battles.py
@@ -6,7 +6,9 @@ pokemon_json = requests.get(pokemon_url).text
 
 print(pokemon_json)
 pokemon = json.loads(pokemon_json)
-#print(pokemon['results'])
-#print(len(pokemon['results']))
+# print(pokemon['results'])
+# print(len(pokemon['results']))
 for result in pokemon['results']:
-    print(result['name'], result['url'])
+    res_json = requests.get(result['url']).json()
+    # print(result['name'], json.dumps(res_json, indent=2))
+    print(result['name'], "Weight:", res_json['weight'])
